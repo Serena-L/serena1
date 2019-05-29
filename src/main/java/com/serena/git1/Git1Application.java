dcec2628147465d590faa4e1fafefbae0428397a
serena_glb <serena_glb@183.com>
2019-05-29 16:02:12 +0800

    这是我用idea创建并上传到GitHub的第一个项目

---
+++ src/main/java/com/serena/git1/Git1Application.java
@@ -0,0 +1,13 @@
+package com.serena.git1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git1Application.class, args);
+    }
+
+}
